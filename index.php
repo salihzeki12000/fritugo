69dea92bf12c1611c5d61a9c90c0222d53b63cbb
albertirawan <albertirawan@fritugo.com>
2015-10-04 23:29:18 +0700

    tes

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+
+<!--albert test github -->
 <!--[if IE 8]> <html class="ie ie8"> <![endif]-->
 <!--[if IE 9]> <html class="ie ie9"> <![endif]-->
 <!--[if gt IE 9]><!--> <html> <!--<![endif]-->
